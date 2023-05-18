f2c224005736a03a80c71c888d228a6b4bafc91c
Ekaterina Ekimova <psich.kawai@gmail.com>
2023-05-18 17:21:40 +0300

    install material ui & add divider

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Divider from '@mui/material/Divider';
 import logo from './Group 1.png';
 import style from './Header.module.css';
 
 function Header(): JSX.Element {
   return (
-    <div className={style.container}>
-      <img className={style.logo} src={logo} alt="logo Landville Group" />
-      <nav className={style.nav}>
-          <Link to="/ourservices" className={style.link}>Our Services</Link>
-          <Link to="/landsupplier" className={style.link}>LandSupplier</Link>
-          <Link to="/landville" className={style.link}>LandVille</Link>
-      </nav>
-      <button type="button" className={style.btn}>Contact Us</button>
-    </div>
+    <>
+      <div className={style.container}>
+        <img className={style.logo} src={logo} alt="logo Landville Group" />
+        <nav className={style.nav}>
+            <Link to="/ourservices" className={style.link}>Our Services</Link>
+            <Link to="/landsupplier" className={style.link}>LandSupplier</Link>
+            <Link to="/landville" className={style.link}>LandVille</Link>
+        </nav>
+        <button type="button" className={style.btn}>Contact Us</button>
+      </div>
+      <Divider className={style.divider} />
+    </>
   );
 }
 
