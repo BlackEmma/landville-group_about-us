da4e76e3e7d37bd40534a576eea4cb4acf1fc89a
Ekaterina Ekimova <psich.kawai@gmail.com>
2023-05-18 22:34:07 +0300

    add some styles fo ContactForm

--- src/features/ContactForm/ContactForm.tsx
+++ src/features/ContactForm/ContactForm.tsx
@@ -1,8 +1,8 @@
+/* eslint-disable @typescript-eslint/quotes */
 /* eslint-disable import/no-extraneous-dependencies */
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
-import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
@@ -12,6 +12,25 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Input from '@mui/material/Input';
+import { styled } from '@mui/material/styles';
+import style from './ContactForm.module.css';
+
+const BootstrapDialog = styled(Dialog)(() => ({
+  '& .MuiPaper-root': {
+    background: '#27282D',
+    width: 540,
+    height: 669,
+    border: '1 solid rgba(255, 255, 255, 0.05)',
+    boxShadow: '0 0 40 rgba(0, 0, 0, 0.1)',
+    borderRadius: 12,
+  },
+  '& .MuiDialogActions-root': {
+    padding: '0',
+  },
+}));
 
 export default function ContactForm(): JSX.Element {
   const [open, setOpen] = React.useState(false);
@@ -29,67 +48,176 @@ export default function ContactForm(): JSX.Element {
       <Button variant="contained" onClick={handleClickOpen}>
         Contact Us
       </Button>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Contact Us</DialogTitle>
+      <BootstrapDialog
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle>
+          <div className={style.formName}>Contact Us</div>
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
         <Divider />
         <DialogContent>
-          <DialogContentText>
+          <DialogContentText
+            sx={{
+              fontFamily: `'Segoe UI', Tahoma, Geneva, Verdana, sans-serif`,
+              fontStyle: 'normal',
+              fontWeight: 600,
+              fontSize: '14px',
+              lineHeight: '20px',
+              color: '#FFFFFF',
+            }}
+          >
             Your Name
-            <span> *</span>
+            <span className={style.star}> *</span>
           </DialogContentText>
-          <TextField
-            margin="dense"
+          <Input
             id="name"
             type="text"
-            variant="outlined"
             placeholder="Your Name"
+            disableUnderline
+            sx={{
+              background: 'rgba(255, 255, 255, 0.1)',
+              border: '1 solid rgba(255, 255, 255, 0.05)',
+              borderRadius: '6px',
+              width: '480px',
+              height: '44px',
+              padding: '11px 16px 13px',
+              margin: '16px 0 20px',
+            }}
           />
-          <DialogContentText>
+          <DialogContentText
+            sx={{
+              fontFamily: `'Segoe UI', Tahoma, Geneva, Verdana, sans-serif`,
+              fontStyle: 'normal',
+              fontWeight: 600,
+              fontSize: '14px',
+              lineHeight: '20px',
+              color: '#FFFFFF',
+            }}
+          >
             Your Email
-            <span> *</span>
+            <span className={style.star}> *</span>
           </DialogContentText>
-          <TextField
-            margin="dense"
+          <Input
             id="email"
             type="email"
-            variant="outlined"
             placeholder="Your Email"
+            disableUnderline
+            sx={{
+              background: 'rgba(255, 255, 255, 0.1)',
+              border: '1 solid rgba(255, 255, 255, 0.05)',
+              borderRadius: '6px',
+              width: '480px',
+              height: '44px',
+              padding: '11px 16px 13px',
+              margin: '16px 0 20px'
+            }}
           />
-          <DialogContentText>
+          <DialogContentText
+            sx={{
+              fontFamily: `'Segoe UI', Tahoma, Geneva, Verdana, sans-serif`,
+              fontStyle: 'normal',
+              fontWeight: 600,
+              fontSize: '14px',
+              lineHeight: '20px',
+              color: '#FFFFFF',
+            }}
+          >
             Phone Number
           </DialogContentText>
-          <InputGroup className="mb-3">
+          <InputGroup
+            className="mb-3"
+            style={{
+              background: 'rgba(255, 255, 255, 0.1)',
+              borderRadius: '6px',
+              margin: '16px 0 20px',
+              width: '480px',
+              height: '44px',
+            }}
+          >
             <DropdownButton
               variant="outline-secondary"
               title="🇺🇸"
               id="input-group-dropdown-1"
             >
-              <Dropdown.Item href="#">Action</Dropdown.Item>
-              <Dropdown.Item href="#">Another action</Dropdown.Item>
-              <Dropdown.Item href="#">Something else here</Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item href="#">Separated link</Dropdown.Item>
+              <Dropdown.Item>Action</Dropdown.Item>
+              <Dropdown.Item>Another action</Dropdown.Item>
             </DropdownButton>
-            <Form.Control aria-label="Text input with dropdown button" placeholder="+1" />
+            <Form.Control
+              aria-label="Text input with dropdown button"
+              style={{
+                background: 'rgba(255, 255, 255, 0.01)',
+                border: '1px solid rgba(255, 255, 255, 0.05)',
+              }}
+              value="+1"
+            />
           </InputGroup>
-          <DialogContentText>
+          <DialogContentText
+            sx={{
+              fontFamily: `'Segoe UI', Tahoma, Geneva, Verdana, sans-serif`,
+              fontStyle: 'normal',
+              fontWeight: 600,
+              fontSize: '14px',
+              lineHeight: '20px',
+              color: '#FFFFFF',
+            }}
+          >
             Your Message
           </DialogContentText>
-          <TextField
-            margin="dense"
+          <Input
             id="message"
             type="text"
             multiline
-            rows={3}
-            variant="outlined"
+            rows={4}
             placeholder="Your Message"
+            disableUnderline
+            sx={{
+              background: 'rgba(255, 255, 255, 0.1)',
+              border: '1 solid rgba(255, 255, 255, 0.05)',
+              borderRadius: '6px',
+              width: '480px',
+              height: '120px',
+              padding: '11px 16px 13px',
+              margin: '16px 0'
+            }}
           />
-          {/* <Form.Control as="textarea" rows={3} /> */}
         </DialogContent>
         <DialogActions>
-          <Button variant="contained" onClick={handleClose}>Submite</Button>
+          <Button
+            variant="contained"
+            onClick={handleClose}
+            sx={{
+              background: '#264EB4',
+              borderRadius: '4px',
+              width: '480px',
+              height: '44px',
+              padding: '10px 24px',
+              margin: '0 30px 30px',
+              fontFamily: `'Segoe UI', Tahoma, Geneva, Verdana, sans-serif`,
+              fontStyle: 'normal',
+              fontWeight: 700,
+              fontSize: '16px',
+              lineHeight: '24px',
+              color: '#FFFFFF',
+              textTransform: 'none'
+            }}
+          >
+            Submit
+          </Button>
         </DialogActions>
-      </Dialog>
+      </BootstrapDialog>
     </div>
   );
 }
