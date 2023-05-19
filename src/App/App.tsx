9f7621f1eae5accb3639006b5160d051d629e850
Ekaterina Ekimova <psich.kawai@gmail.com>
2023-05-19 18:22:01 +0300

    add Footer & Divider to App.tsx

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Divider from '@mui/material/Divider';
 import Header from '../features/Header/Header';
 import Main from '../features/Main/Main';
+import Footer from '../features/Footer/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App(): JSX.Element {
@@ -20,6 +21,14 @@ function App(): JSX.Element {
         }}
       />
       <Main />
+      <Divider
+        sx={{
+          width: '100%',
+          height: '2px',
+          background: 'rgba(255, 255, 255, 0.2)',
+        }}
+      />
+      <Footer />
     </div>
   );
 }
